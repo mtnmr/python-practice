ba1995d857c4ff6b93df2459985fcc73337d5fb0
mtnmr <mei.rila1013@gmail.com>
2021-07-13 16:46:30 +0900

    ゾロ目

---
+++ src/5-2-1,2.py
@@ -0,0 +1,38 @@
+import math
+
+N = int(input())
+
+x = math.ceil(N/9)
+
+y = N % 9 
+if y == 0:
+    y = 9
+
+ans = ""
+
+for _ in range (0,x):
+    ans += sty(y)
+
+print(ans)
+
+
+
+N = int(input())
+
+z = 0
+
+for i in range(1,55555+1):
+    si = str(i)
+
+    ok = True
+
+    for s in si:
+        if s != si[0]:
+            ok = False
+        if ok:
+            z += 1
+        if ok and z == N:
+            ans = i
+
+print (ans)
+
