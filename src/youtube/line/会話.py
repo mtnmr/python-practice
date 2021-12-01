79d26e89762de01b49cbb7e5db3c0cc068ba6abf
mtnmr <mei.rila1013@gmail.com>
2021-12-01 09:30:51 +0900

    会話保存

--- src/youtube/line/会話.py
+++ src/youtube/line/会話.py
@@ -1,26 +1,63 @@
+
+
 from flask import Flask, request, abort
 
 from linebot import (
-   LineBotApi, WebhookHandler
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-   InvalidSignatureError
+    InvalidSignatureError
 )
 from linebot.models import (
-   MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,
 )
 
 import os
 import requests
+import pprint
+import pya3rt
 
 app = Flask(__name__)
 
-TALKAPI_KEY = os.environ['YOUR_API']
+TALKAPI_KEY = os.environ['apikey']
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+
+
+@app.route("/")
+def hello_world():
+   return "hello world!"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    push_text = event.message.text
+    msg = talkapi(push_text)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=msg))
 
 def talkapi(text):
    url = 'https://api.a3rt.recruit.co.jp/talk/v1/smalltalk'
@@ -33,37 +70,17 @@ def talkapi(text):
    msg = data['results'][0]['reply']
    return msg
 
-@app.route("/")
-def hello_world():
-   return "hello world!"
+# def talkapi(text):
+#     client = pya3rt.TalkClient(TALKAPI_KEY)
+#     response = client.talk(text)
 
-@app.route("/callback", methods=['POST'])
-def callback():
-   # get X-Line-Signature header value
-   signature = request.headers['X-Line-Signature']
+#     data =  response.json()
+#     if data['status'] != 0:
+#         return data['message']
 
-   # get request body as text
-   body = request.get_data(as_text=True)
-   app.logger.info("Request body: " + body)
-
-   # handle webhook body
-   try:
-       handler.handle(body, signature)
-   except InvalidSignatureError:
-       print("Invalid signature. Please check your channel access token/channel secret.")
-       abort(400)
-
-   return 'OK'
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-   push_text = event.message.text
-   msg = talkapi(push_text)
-   line_bot_api.reply_message(
-       event.reply_token,
-       TextSendMessage(text=msg))
+#     msg = data['results'][0]['reply']
+#     return msg
 
 if __name__ == "__main__":
-   port = int(os.getenv("PORT"))
-   app.run(host="0.0.0.0", port=port)
-
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
