667e72217b57add11910d0e43b615a040a5df978
mtnmr <mei.rila1013@gmail.com>
2022-08-08 11:52:20 +0900

    PAST本まとめ

--- src/PAST6/6-4-1.py
+++ src/PAST6/6-4-1.py
@@ -8,3 +8,33 @@ for i in range(2,n):
     cost[i] = min(cost[i-2]+abs(h[i-2]-h[i]), cost[i-1]+abs(h[i-1]-h[i]))
 
 print(cost[n-1])
+
+
+#メモ化再帰
+import sys
+sys.setrecursionlimit(1000000)
+
+n = int(input())
+h = list(map(int,input().split()))
+
+cost = [0] * n
+
+#cost[i]が計算ずみかどうか
+done = [False] * n
+
+def rec(i):
+    if done[i]:
+        return cost[i]
+    
+    if i == 0:
+        cost[i] = 0
+    elif i == 1:
+        cost[i] = rec(0) + abs(h[0]-h[1])
+    else:
+        cost[i] = min(rec[i-1]+abs(h[i-1]-h[i]), rec(i-2)+abs(h[i-2]-h[i]))
+
+    done[i] = True
+    return cost[i]
+
+print(rec(n-1))
+
